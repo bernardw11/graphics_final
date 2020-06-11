78077867caf3a33dbabfccb2b147c6c022cb923f
bernardw11 <bwang00@stuy.edu>
2020-06-11 13:59:11 -0400

    oh bruh

--- display.py
+++ display.py
@@ -10,8 +10,8 @@ RED = 0
 GREEN = 1
 BLUE = 2
 
-DEFAULT_COLOR = [0, 0, 0]
-
+#DEFAULT_COLOR = [0, 0, 0]
+DEFAULT_COLOR = [255, 228, 138]
 def new_screen( width = XRES, height = YRES ):
     screen = []
     for y in range( height ):
